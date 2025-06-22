3c97c704084082e45f7d6f8f26d1996d57c2567a
Salman Shaikh <salmans3724@gmail.com>
2025-06-22 15:47:57 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Education from './components/Education'
+import Extracurriculars from './components/Extracurriculars'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Education />
+      <Extracurriculars />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
